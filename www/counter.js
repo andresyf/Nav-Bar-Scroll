d079a1803e4cc09f2795ccbc9b4c26eacfe4eef8
Andrés Yáñez <ndrs.yanez@gmail.com>
2020-11-12 11:16:44 +0100

    counter

---
+++ www/counter.js
@@ -0,0 +1,20 @@
+let counts = document.querySelectorAll('.count');
+
+counts.forEach((count) => {
+    const updateCount = () => {
+
+        let currentCount = parseInt(count.innerHTML);
+        let targetCount = count.getAttribute('data-target');
+        let increaseSpeed = 40;
+        let increaseCount = parseInt(targetCount / increaseSpeed);
+
+        if (currentCount < targetCount) {
+            count.innerText = currentCount + increaseCount;
+        } else {
+            count.innerText = targetCount;
+        }
+        setTimeout(updateCount,increaseSpeed);
+    }
+
+    updateCount();
+})
